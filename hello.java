74eae72add05744dbf651d73bf4d34d8861fe39b
moorthymj <59924901+moorthymj@users.noreply.github.com>
2020-01-15 09:50:37 -0500

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,20 @@
+/*
+* Lab 1
+* Madhav Moorthy
+* Jan 15, 2020
+* CMSC 255 Section 3
+*/
+
+public class hello
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Madhav Moorthy");
+		System.out.println("Lab 1");
+		System.out.println("Intro to Course for Mac");
+		System.out.println("Jan 15, 2020");
+		System.out.println("CMSC 255 Section 3");
+		System.out.println();
+
+	}
+}
